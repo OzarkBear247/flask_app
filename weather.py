18a5052d31b5c104a85173019a6bee3cae9d5745
OzarkBear247 <kstuart70@gmail.com>
2017-04-13 13:34:28 -0400

    Second commit

--- weather.py
+++ weather.py
@@ -12,7 +12,12 @@ def get_weather(address):
     latitude = location.latitude
     longitude = location.longitude
     forecast = forecastio.load_forecast(api_key, latitude, longitude).currently()
+    byDaily = forecastio.load_forecast(api_key, latitude, longitude).daily()
     summary = forecast.summary
     temperature = forecast.temperature
-    return "{} and {}° at {}".format(summary, temperature, address)
+    for hourlyData in byDaily.data:
+        return "Today will be {} with a High of {}°  and a Low of {}°  Humidity will be {} with a DewPoint of {}".format(forecast.summary, hourlyData.temperatureMax, hourlyData.temperatureMin, hourlyData.humidity, hourlyData.dewPoint)
+       
+
+
 
